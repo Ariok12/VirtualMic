c098348f9c676cdae1de60e7d7d47c0e79a8e6ac
Ariok12 <ariok12@users.noreply.github.com>
2026-06-06 18:45:23 +0530

    chore: bump version to 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ariok12.virtualmic"
         minSdk = 31
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testProguardFiles("test-proguard-rules.pro")
